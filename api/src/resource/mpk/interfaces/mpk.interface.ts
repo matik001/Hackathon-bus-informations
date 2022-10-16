132a7df2ca6e668e13f988a61b4e6e2445ed1511
jakub-galecki <324384@uwr.edu.pl>
2022-10-16 04:50:06 +0200

    get stoooops

--- api/src/resource/mpk/interfaces/mpk.interface.ts
+++ api/src/resource/mpk/interfaces/mpk.interface.ts
@@ -1,6 +1,7 @@
 import { VehicleType } from '../types/vehicle-info.type';
 
 export interface MpkInterface {
+  getStops(): Promise<Array<string>>;
   getBusesForStop(stop_name: string): Promise<Array<string>>;
   getBusesLocationForStop(stop_name: string): Promise<Array<VehicleType>>;
 }
